17f4fc8ff1f03c70eb1b7c3c36d40fee3648e094
Alon Regev <alonr3704@gmail.com>
2022-05-11 23:13:11 +0300

    added sigmoid to nn test

--- Tests/NeuralNetwork.py
+++ Tests/NeuralNetwork.py
@@ -2,7 +2,7 @@ import numpy
 import random
 import matplotlib.pyplot as plt
 
-LEARNING_RATE = 0.01
+LEARNING_RATE = 0.5
 
 def main():
     # layers: [2 inputs, 4 hidden, 1 output]
@@ -10,24 +10,28 @@ def main():
     weight = 0
     bias = 0
 
-    data = {(x / 100): (x / 200) + random.random() / 50 for x in range(50)}
+    data = {(x / 10 - 8): sigmoid(x / 20 - 5 + random.random()) for x in range(200)}
 
     def get_output(input):
-        return weight * input + bias
+        return sigmoid(weight * input + bias)
 
-    for iteration in range(10000):
+    for iteration in range(100000):
         # calculate derivatives
-        # output = weight * x + bias
+        # f = weight * x + bias
+        # output = s(f)
         # error = (output - y) ^ 2
-        # dE/dp = 2 * (output - y) * df/dp
-        # dE/dw = 2 * (output - y) * x
-        # dE/db = 2 * (output - y)
+        # dE/dp = dE/dout * dout/df * df/dp
+        #       = 2 * (output - y) * s'(f) * df/dp
+        # dE/dw = 2 * (output - y) * s'(f) * x
+        # dE/db = 2 * (output - y) * s'(f)
 
         # get output
         x, y = random.choice(list(data.items()))
-        output = get_output(x)
-        dw = 2 * (output - y) * x
-        db = 2 * (output - y)
+        f = weight * x + bias
+        sdf = sigmoid_derivative(f)
+        output = sigmoid(f)
+        db = 2 * (output - y) * sdf
+        dw = db * x
         # update parameters
         weight -= dw * LEARNING_RATE
         bias -= db * LEARNING_RATE
@@ -44,6 +48,12 @@ def main():
     plt.scatter(x_values, y_values)
     plt.plot(x_values, [get_output(x) for x in x_values], color="orange")
     plt.show()
+
+def sigmoid(x):
+    return 1 / (1 + numpy.exp(-x))
+
+def sigmoid_derivative(x):
+    return sigmoid(x) * sigmoid(1 - x)
 
 if __name__ == "__main__":
     main()
