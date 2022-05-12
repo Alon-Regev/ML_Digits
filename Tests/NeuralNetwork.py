65fb2923009c621b82d6eb4afdc013d53e177a17
Alon Regev <alonr3704@gmail.com>
2022-05-12 16:31:15 +0300

    multiple inputs

--- Tests/NeuralNetwork.py
+++ Tests/NeuralNetwork.py
@@ -2,52 +2,56 @@ import numpy
 import random
 import matplotlib.pyplot as plt
 
-LEARNING_RATE = 0.5
+LEARNING_RATE = 0.1
 
 def main():
     # layers: [2 inputs, 4 hidden, 1 output]
-    layers_sizes = [1, 1]
-    weight = 0
+    layers_sizes = [2, 1]
+    weights = [0, 0]
     bias = 0
 
-    data = {(x / 10 - 8): sigmoid(x / 20 - 5 + random.random()) for x in range(200)}
+    def get_data():
+        inputs = [random.random() / 3 + 1/12 for _ in range(2)]
+        output = sum(inputs)
+        return (inputs, output)
 
-    def get_output(input):
-        return sigmoid(weight * input + bias)
+    def get_output(inp):
+        return sigmoid(sum([weights[j] * inp[j] for j in range(2)]) + bias)
 
-    for iteration in range(100000):
+    for iteration in range(50000):
         # calculate derivatives
-        # f = weight * x + bias
+        # f = sum(weight * i) + bias
         # output = s(f)
-        # error = (output - y) ^ 2
+        # error = sum((output - y) ^ 2
         # dE/dp = dE/dout * dout/df * df/dp
         #       = 2 * (output - y) * s'(f) * df/dp
         # dE/dw = 2 * (output - y) * s'(f) * x
         # dE/db = 2 * (output - y) * s'(f)
 
         # get output
-        x, y = random.choice(list(data.items()))
-        f = weight * x + bias
+        inp, out = get_data()
+        f = sum([weights[j] * inp[j] for j in range(2)]) + bias
         sdf = sigmoid_derivative(f)
         output = sigmoid(f)
-        db = 2 * (output - y) * sdf
-        dw = db * x
         # update parameters
-        weight -= dw * LEARNING_RATE
+        db = 2 * (output - out) * sdf
         bias -= db * LEARNING_RATE
+        for j in range(len(weights)):
+            weights[j] -= db * inp[j] * LEARNING_RATE
 
     # calculate error
     error = 0
-    for x, y in data.items():
-        error += (get_output(x) - y) ** 2
-    print(error)
+    for _ in range(1000):
+        d = get_data()
+        error += (get_output(d[0]) - d[1]) ** 2
+    print("Error:", error)
 
-    # plot
-    x_values = list(data.keys())
-    y_values = list(data.values())
-    plt.scatter(x_values, y_values)
-    plt.plot(x_values, [get_output(x) for x in x_values], color="orange")
-    plt.show()
+    # play with result
+    while True:
+        a = float(input("Enter in1: "))
+        b = float(input("Enter in2: "))
+        print("result:", get_output([a, b]))
+    
 
 def sigmoid(x):
     return 1 / (1 + numpy.exp(-x))
